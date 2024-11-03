3479f5566723fbda1ff1821be06fbb999a0385e0
parikshitlfj12 <parikshitlfj@gmail.com>
2024-11-03 22:55:31 +0530

    chore: ssl protection configurations

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from './config/configuration';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+
+const { nodeEnv } = configuration.appConfig;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions =
+    nodeEnv === 'production'
+      ? {
+          key: fs.readFileSync(
+            '/etc/letsencrypt/live/api.oggames.fun/privkey.pem',
+          ),
+          cert: fs.readFileSync(
+            '/etc/letsencrypt/live/api.oggames.fun/cert.pem',
+          ),
+        }
+      : undefined;
+
+  const app = await NestFactory.create(
+    AppModule,
+    httpsOptions ? { httpsOptions } : {},
+  );
   app.enableCors({
     origin: configuration.appConfig.allowedOrigins,
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'HEAD', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
